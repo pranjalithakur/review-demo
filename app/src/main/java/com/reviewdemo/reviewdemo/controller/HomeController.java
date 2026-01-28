1e1b81bd492e64d78ee44cb55b73f8be950d3b61
Pranjali Thakur <47360190+pranjalithakur@users.noreply.github.com>
2026-01-28 10:09:27 -0500

    add application code

---
+++ app/src/main/java/com/reviewdemo/reviewdemo/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.reviewdemo.reviewdemo.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.reviewdemo.reviewdemo.utils.Utils;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String renderGet(Model model, HttpServletRequest request, HttpServletResponse response) {
+		if (Utils.getSessionUserName(request, response) != null) {
+			return Utils.redirect("feed");
+		}
+
+		model.addAttribute("javax.servlet.forward.request_uri", "/login");
+		model.addAttribute("username", "");
+
+		return "login";
+	}
+}
